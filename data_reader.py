bb82415f29f0a9a3e473bd4b34416100dda9a89e
Matthew <mkerner3@gatech.edu>
2023-06-01 15:53:35 -0400

    Implemented test.py logic into data_reader.py

--- data_reader.py
+++ data_reader.py
@@ -7,7 +7,7 @@ import keyboard
 
 #Arduino set to COM5
 #If this errors, switch the value next to COM with what COM port you are talking on.
-ser = serial.Serial('COM5', 9600, timeout=1)
+ser = serial.Serial('COM5', 250000, timeout=1)
 
 #Variable/Data structure initialization
 #time.sleep(2)
@@ -16,8 +16,13 @@ sr = 30000
 data = deque() #Pythons version of a linked list.
 times = deque()
 cont = True
-start_time = time.time()
-prev_time = time.time()
+start_time = time.perf_counter()
+#prev_time = time.time()
+
+fft_of_sample = None
+fft_amplitudes = None
+fft_phases = None
+harmonics = None
 
 # Not the most elegant solution, but works to stop data reading for now.
 def on_key_press(event):
@@ -28,14 +33,9 @@ def on_key_press(event):
 keyboard.on_press(on_key_press)
 
 while cont:
-    current = time.time()
-    current_time = current - start_time
-    #print(current_time)
-    elapsed_time = current_time - prev_time
+    current_time = time.perf_counter() - start_time
 
     times.append(current_time)
-    if len(times) > num_pts:
-        r_head = times.popleft()
 
     line = ser.readline() #read a byte
     if line:
@@ -48,31 +48,32 @@ while cont:
                 data.append(num) 
             except ValueError:
                 continue #Rejects bad string and continues while loop
+            
+            print(len(data))
 
-            #TODO Implement fourier transform calculations from test.py
-            fft_of_sample = np.fft.fft(data)
-            fft_amplitudes = 2.0 / num_pts * np.abs(fft_of_sample)
-            print(len(fft_amplitudes))
-            fft_phases = np.angle(fft_of_sample)
-            harmonics = np.arange(1, 250)
+            if len(times) != 0 and len(data) > num_pts:
+                data.popleft()
+                times.popleft()
+            
+            end_time = current_time + 1/sr
+            while time.perf_counter() < end_time:
+                pass
 
-        if len(data) > num_pts: #Update array with new values and delete old values
-            r_head = data.popleft() #r_head temporarily stores the replaced old value.
+            #TODO is the fourier functionality the same with varying numbers of points?
+            #data_copy = np.array(data)
+            #fft_of_sample = np.fft.fft(data_copy)
+            #fft_amplitudes = 2.0 / num_pts * np.abs(fft_of_sample)
+            #print(len(fft_amplitudes))
+            #fft_phases = np.angle(fft_of_sample)
+            #harmonics = np.arange(1, 250)
 
-        
-
-        prev_time = current_time
-
-        #TODO implement lowpass filter
-        
-        #fig1,ax1 = plt.subplots()
-        #ax1.plot(t,data)
-        #ax1.set_title('Raw data')
-        
-        #fig2,ax2 = plt.subplots()
-        #ax2.bar(harmonics, fft_amplitudes[harmonics])
-        #ax2.set_title('Fourier amplitudes of raw data')
-
+            if len(data) == num_pts:
+                data_copy = np.array(data)
+                fft_of_sample = np.fft.fft(data_copy)
+                fft_amplitudes = 2.0 / num_pts * np.abs(fft_of_sample)
+                print(len(fft_amplitudes))
+                fft_phases = np.angle(fft_of_sample)
+                harmonics = np.arange(1, 250)
 
 keyboard.unhook_all()
 
