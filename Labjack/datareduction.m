5fd93e89ed9d285edc74015f79e0edd7e18fbf39
Matthew <mkerner3@gatech.edu>
2024-12-08 18:07:43 -0500

    matlab post processing

--- Labjack/datareduction.m
+++ Labjack/datareduction.m
@@ -14,21 +14,38 @@ disp(matrix_1);
 %}
 % Load the .mat file
 loaded_data = load('AF_manip_data.mat');
+tare_data = load('tare_manip_data.mat');
 
 % Access the 3D array from the loaded data
 data_array = loaded_data.cell_array; % Assuming the 35x150x4 data is stored in 'cell_array'
+tare_array = tare_data.cell_array;
 
 % Initialize arrays to store sensor data
 num_sensors = size(data_array, 3); % Number of sensors (columns in each matrix)
 num_matrices = size(data_array, 1); % Number of matrices (35 in this case)
 sensor_data = zeros(num_matrices, num_sensors);
+tare_data = zeros(num_matrices, num_sensors);
 
-% Extract the first row of each matrix for each sensor
 for i = 1:num_matrices
     for sensor = 1:num_sensors
-        sensor_data(i, sensor) = data_array(i, 1, sensor); % First row of matrix, column = sensor
+        tare_samples = squeeze(tare_array(i, :, sensor));
+        tare_data(i, sensor) = mean(tare_samples - 2.5);
     end
 end
+
+for i = 1:num_matrices
+    for sensor = 1:num_sensors
+        sensor_data(i, sensor) = mean(data_array(i, :, sensor));
+    end
+end
+
+sensor_data = sensor_data - tare_data;
+
+diffP = 1000*(sensor_data-2.5);
+
+rho = 1.204; % Density for air
+
+veloc = sqrt(2*diffP / rho);
 
 % Plot each sensor's data
 figure;
@@ -36,8 +53,28 @@ for sensor = 1:num_sensors
     subplot(num_sensors, 1, sensor); % Create a subplot for each sensor
     plot(sensor_data(:, sensor), 'o-', 'LineWidth', 1.5); % Plot data for the sensor
     title(['Sensor ', num2str(sensor)]);
-    xlabel('Matrix Index');
+    xlabel('Slew Position');
     ylabel('Sensor Value');
+    grid on;
+end
+
+figure;
+for sensor = 1:num_sensors
+    subplot(num_sensors, 1, sensor); % Create a subplot for each sensor
+    plot(diffP(:, sensor), 'o-', 'LineWidth', 1.5); % Plot data for the sensor
+    title(['Sensor ', num2str(sensor)]);
+    xlabel('Slew Position');
+    ylabel('Differential Pressure (Pa)');
+    grid on;
+end
+
+figure;
+for sensor = 1:num_sensors
+    subplot(num_sensors, 1, sensor); % Create a subplot for each sensor
+    plot(veloc(:, sensor), 'o-', 'LineWidth', 1.5); % Plot data for the sensor
+    title(['Sensor ', num2str(sensor)]);
+    xlabel('Slew Position');
+    ylabel('Velocity (m/s)');
     grid on;
 end
 
